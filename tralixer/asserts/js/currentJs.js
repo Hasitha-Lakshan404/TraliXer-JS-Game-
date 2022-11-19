1e0320f3d3fbf6b10bed3444418ea48368169c9a
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-11-19 15:34:42 +0530

    Created Game Object with Player

--- tralixer/asserts/js/currentJs.js
+++ tralixer/asserts/js/currentJs.js
@@ -3,7 +3,7 @@ $(window).on('load',function () {
     //canvas Setup
     const canvas=document.getElementById('canvas1');
     const ctx =canvas.getContext('2d');
-    canvas.width=1500;
+    canvas.width=500;
     canvas.height=500;
 
     /*===== Track Specified user Inputs =====*/
@@ -23,7 +23,24 @@ $(window).on('load',function () {
 
     /*===== Control main Character /player Sprites =====*/
     class Player{
+        constructor(game) {
+            this.game=game;
+            this.width=120;
+            this.height=190;
+            this.x=20;
+            this.y=100;
+            this.speedY=0;
+        }
 
+        /*== for the player movement ==*/
+        update(){
+            this.y+=this.speedY;
+        }
+
+        /*== for the draw graphics ==*/
+        draw(context){ //context -> which canvas element  we want to draw
+            context.fillRect(this.x,this.y,this.width,this.height);
+        }
 
     }
 
@@ -48,9 +65,21 @@ $(window).on('load',function () {
     }
 
     /*===== All Game Logic =====*/
-    class game{
+    class Game{
+        constructor(width,height) {
+            this.width=width;
+            this.height=height;
+            this.player=new Player(this); //Create new Player Object and parse 'game' object to it
+        }
+        update(){
+            this.player.update();
+        }
 
+        draw(context){ //to select which canvas
+            this.player.draw(context);
+        }
     }
 
+    const game=new Game(canvas.width,canvas.height);
 
 });
