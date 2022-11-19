6c6000a0dea0edaa7e117a1ed1d744e9fa5dd9c3
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-11-19 14:40:44 +0530

    Inroduce Classes for all Action/events

--- tralixer/asserts/js/currentJs.js
+++ tralixer/asserts/js/currentJs.js
@@ -6,4 +6,50 @@ $(window).on('load',function () {
     canvas.width=1500;
     canvas.height=500;
 
+    /*===== Track Specified user Inputs =====*/
+    class InputHandler{
+
+    }
+
+    /*===== Handle Player Lasers =====*/
+    class ProjectTitle{
+
+    }
+
+    /*===== Handle Falling Things that come from damaged enemies =====*/
+    class Particle{
+
+    }
+
+    /*===== Control main Character /player Sprites =====*/
+    class Player{
+
+    }
+
+    /*===== Handling Enemy types =====*/
+    class Enemy{
+
+    }
+
+    /*===== Handle individual Background Layers =====*/
+    class Layer{
+
+    }
+
+    /*===== Handle All 'Layer Object' together to animate entire game =====*/
+    class Background{
+
+    }
+
+    /*===== Draw Score timer ane other Display Information =====*/
+    class Ui{
+
+    }
+
+    /*===== All Game Logic =====*/
+    class game{
+
+    }
+
+
 });
