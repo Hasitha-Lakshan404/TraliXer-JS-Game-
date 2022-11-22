5aae50e4afab3fa6b09afc0808189a7c71d5b84b
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-11-22 22:58:50 +0530

    Create Bullet/lasers  Support class and set fillStyle to the player context

--- tralixer/asserts/js/currentJs.js
+++ tralixer/asserts/js/currentJs.js
@@ -30,6 +30,25 @@ $(window).on('load',function () {
 
     /*===== Handle Player Lasers =====*/
     class ProjectTitle{
+        constructor(game,x,y) {
+            this.game=game;
+            this.x=x;
+            this.y=y;
+            this.width=10;
+            this.height=3;
+            this.speed=3;
+            this.markedForDeletion=false;
+
+        }
+        update(){
+            this.x+=this.speed;
+            if(this.x>this.game.width*0.8)this.markedForDeletion=true;
+        }
+
+        draw(context){
+            context.fillStyle='yellow';
+            context.fillRect(this.x,this.y,this.width,this.height);
+        }
 
     }
 
@@ -65,6 +84,7 @@ $(window).on('load',function () {
 
         /*== for the draw graphics ==*/
         draw(context){ //context -> which canvas element  we want to draw
+            context.fillStyle='black'
             context.fillRect(this.x,this.y,this.width,this.height);
         }
 
